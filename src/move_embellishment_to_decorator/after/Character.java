757329eeaaf068aeedc1b044d3724f29d2b5f477
Bill Schofield <bill@billschofield.net>
2013-08-03 12:47:15 -0500

    Move embellishment to decorator

---
+++ src/move_embellishment_to_decorator/after/Character.java
@@ -0,0 +1,20 @@
+package move_embellishment_to_decorator.after;
+
+import move_embellishment_to_decorator.Position;
+
+public class Character implements Actor {
+
+    private Position position;
+
+    public Character(Position position){
+        this.position = position;
+    }
+
+    public void moveTo(Position newPosition){
+        position = newPosition;
+    }
+
+    public void attack(Actor target){
+
+    }
+}
