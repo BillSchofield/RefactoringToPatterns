7757b983bed8ef03ac39618f22adad44a02e5151
Bill Schofield <bill@billschofield.net>
2013-08-05 13:29:00 -0500

    Replace state altering conditionals with State

---
+++ src/replace_state_altering_conditionals_with_state/Actor.java
@@ -0,0 +1,16 @@
+package replace_state_altering_conditionals_with_state;
+
+public class Actor {
+    public int hitPoints() {
+        return 0;
+    }
+
+    public void snore() {
+    }
+
+    public void followPath() {
+    }
+
+    public void attackCurrentTarget() {
+    }
+}
